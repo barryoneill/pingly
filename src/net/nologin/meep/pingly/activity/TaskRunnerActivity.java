346a04e94d2bc24c37c7b70f80256c3bb5d3c96a
Barry O'Neill <barra.oneill@gmail.com>
2012-02-15 22:18:58 -0500

    add initial URL fetch stuff

--- src/net/nologin/meep/pingly/activity/TaskRunnerActivity.java
+++ src/net/nologin/meep/pingly/activity/TaskRunnerActivity.java
@@ -1,7 +1,22 @@
 package net.nologin.meep.pingly.activity;
 
 import static net.nologin.meep.pingly.PinglyConstants.LOG_TAG;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.apache.http.Header;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import net.nologin.meep.pingly.R;
+import net.nologin.meep.pingly.StringUtils;
 import net.nologin.meep.pingly.model.PinglyTask;
 import net.nologin.meep.pingly.model.TaskRunResult;
 
@@ -23,11 +38,11 @@ public class TaskRunnerActivity extends BasePinglyActivity {
 	private Button stopTaskBut;
 	private TextView taskLogOutput;
 	private ScrollView taskLogScroller;
-	
+
 	private PinglyTask currentTask;
 
 	private AsyncTaskRunner asyncTask;
-	
+
 	@Override
 	protected void onCreate(Bundle state) {
 
@@ -43,134 +58,158 @@ public class TaskRunnerActivity extends BasePinglyActivity {
 		taskName = (TextView) findViewById(R.id.text_newTask_name);
 		runAgainBut = (Button) findViewById(R.id.but_taskRunner_runAgain);
 		stopTaskBut = (Button) findViewById(R.id.but_taskRunner_cancel);
-		taskLogOutput = (TextView) findViewById(R.id.task_log_output);	
+		taskLogOutput = (TextView) findViewById(R.id.task_log_output);
 		taskLogScroller = (ScrollView) findViewById(R.id.task_log_scroller);
-		
-		runAgainBut.setOnClickListener(new OnClickListener() {			
-			public void onClick(View v) {				
-				clearAndStartTask();				
+
+		runAgainBut.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				clearAndStartTask();
 			}
 		});
-		
-		stopTaskBut.setOnClickListener(new OnClickListener() {			
+
+		stopTaskBut.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
-				if(asyncTask != null && !asyncTask.isCancelled()){
+				if (asyncTask != null && !asyncTask.isCancelled()) {
 					asyncTask.cancel(true);
 				}
 			}
 		});
-		
+
 		// init view
 		taskName.setText(currentTask.name);
-	
-//		// after activity restart (screen rotate)
-//		if(state != null && state.containsKey("taskLogOutput")){
-//			taskLogOutput.setText(state.getString("taskLogOutput"));
-//			// hack to push to bottom _after_ scrollview resize
-//			taskLogScroller.post(new Runnable() {
-//		         public void run() {
-//		             ((ScrollView) findViewById(R.id.task_log_scroller)).fullScroll(View.FOCUS_DOWN);
-//		         }
-//		 });
-//		}
-		
-		if(asyncTask == null || asyncTask.getStatus() == AsyncTask.Status.FINISHED){			
+
+		// // after activity restart (screen rotate)
+		// if(state != null && state.containsKey("taskLogOutput")){
+		// taskLogOutput.setText(state.getString("taskLogOutput"));
+		// // hack to push to bottom _after_ scrollview resize
+		// taskLogScroller.post(new Runnable() {
+		// public void run() {
+		// ((ScrollView)
+		// findViewById(R.id.task_log_scroller)).fullScroll(View.FOCUS_DOWN);
+		// }
+		// });
+		// }
+
+		if (asyncTask == null
+				|| asyncTask.getStatus() == AsyncTask.Status.FINISHED) {
 			clearAndStartTask();
 		}
-		
+
 	}
-	
-//	@Override
-//	public void onSaveInstanceState(Bundle state) {
-//	
-//		super.onSaveInstanceState(state);
-//		state.putString("taskLogOutput", taskLogOutput.getText().toString());
-//	}
-//	
+
+	// @Override
+	// public void onSaveInstanceState(Bundle state) {
+	//
+	// super.onSaveInstanceState(state);
+	// state.putString("taskLogOutput", taskLogOutput.getText().toString());
+	// }
+	//
 	private void clearAndStartTask() {
-		
+
 		taskLogOutput.setText("");
 
 		asyncTask = new AsyncTaskRunner();
 		asyncTask.execute(currentTask);
 	}
-	
-	
-	
-	private void appendLogLine(String txt){
-				
-		taskLogOutput.append(txt + "\n");		
+
+	private void appendLogLine(String txt) {
+
+		taskLogOutput.append(txt + "\n");
 		// taskLogScroller.smoothScrollTo(0, taskLogOutput.getBottom());
 		taskLogScroller.fullScroll(ScrollView.FOCUS_DOWN);
 	}
-	
-	
-	
-	private class AsyncTaskRunner extends AsyncTask<PinglyTask, String, TaskRunResult> {
 
-		
+	private class AsyncTaskRunner extends
+			AsyncTask<PinglyTask, String, TaskRunResult> {
+
 		@Override
 		protected void onPreExecute() {
 			super.onPreExecute();
-			
+
 			runAgainBut.setEnabled(false);
 			stopTaskBut.setEnabled(true);
-			
-			appendLogLine("Starting task..");
-			
+
+			appendLogLine("Starting async task..");
+
 		}
-		
+
 		@Override
 		protected TaskRunResult doInBackground(PinglyTask... params) {
 
-			publishProgress("background started");			
-			
-			try {
-				for(int i=0;i<100;i++){
-					Thread.sleep(100);
-					publishProgress("At: " + i);
-				}
-			} catch (InterruptedException e) {
-				publishProgress("Interrupted");
-				e.printStackTrace();
+			PinglyTask t = params[0];
+
+			publishProgress("Processing: " + t);
+
+			if (StringUtils.isBlank(t.url)) {
+				publishProgress("No URL specified, nothing to check.");
+				return null;
 			}
+
+			publishProgress("Adding dummy 2 second sleep..");
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e1) {
+			}
+			publishProgress(".. and back");
 			
-			publishProgress("background finished");
 			
+			HttpClient client = new DefaultHttpClient();
+			HttpGet request = new HttpGet();
+			try {
+				request.setURI(new URI(t.url));
+				HttpResponse response = client.execute(request);
+				
+				publishProgress("========== response start ==========");
+				for(Header hdr : response.getAllHeaders()){
+					publishProgress(hdr.getName()  + ": " + hdr.getValue());
+				}
+				publishProgress("========== response end ==========");
+				
+			} catch (Exception e) {
+				publishProgress(e);
+			}
+
 			return null;
 		}
-	
+
+		private void publishProgress(Exception e) {
+			publishProgress("========== exception ==========");
+			StringWriter sw = new StringWriter();
+			e.printStackTrace(new PrintWriter(sw));
+			publishProgress(sw.getBuffer().toString());
+			publishProgress("========== exception ==========");
+		}
+
 		@Override
 		protected void onProgressUpdate(String... values) {
-		
+
 			super.onProgressUpdate(values);
-			
-			for(String val : values){
+
+			for (String val : values) {
 				appendLogLine(val);
 			}
-			
+
 		}
-		
 
 		@Override
 		protected void onCancelled() {
 
 			super.onCancelled();
-			
+
 			appendLogLine("Cancelled");
-			
+
 			runAgainBut.setEnabled(true);
 			stopTaskBut.setEnabled(false);
 		}
-		
+
 		@Override
 		protected void onPostExecute(TaskRunResult result) {
-		
-			appendLogLine("Post Execute");
-			
+
+			appendLogLine("Async task finished.");
+
 			runAgainBut.setEnabled(true);
 			stopTaskBut.setEnabled(false);
-			
+
 		}
 	}
 
