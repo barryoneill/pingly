56973ace6725da78f08aec97ae7fe1804c8522d7
Barry O'Neill <barra.oneill@gmail.com>
2012-03-21 21:45:57 -0400

    date and time picker stubs added

--- src/net/nologin/meep/pingly/activity/ScheduleDetailActivity.java
+++ src/net/nologin/meep/pingly/activity/ScheduleDetailActivity.java
@@ -1,10 +1,14 @@
 package net.nologin.meep.pingly.activity;
 
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.text.format.Time;
 import android.view.ContextThemeWrapper;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.*;
@@ -12,7 +16,11 @@ import net.nologin.meep.pingly.R;
 import net.nologin.meep.pingly.model.IdValuePair;
 import net.nologin.meep.pingly.model.SchedulerRepetitionUnit;
 import net.nologin.meep.pingly.util.PinglyUtils;
-import net.nologin.meep.pingly.view.PinglyBasePrefView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 
 public class ScheduleDetailActivity extends BasePinglyActivity {
 
@@ -23,12 +31,116 @@ public class ScheduleDetailActivity extends BasePinglyActivity {
 
     }
 
+    private Context getDialogContext(){
+        return new ContextThemeWrapper(this, R.style.PinglyDialogTheme);
+    }
+
+    private View inflateScheduleDialogLayout(int layoutId){
+
+        return View.inflate(getDialogContext(), layoutId, (ViewGroup) getCurrentFocus());
+    }
+
+    private AlertDialog.Builder getAlertDialogBuilder() {
+        return new AlertDialog.Builder(getDialogContext());
+    }
+
+    public void configureStartTime(View v) {
+
+        View layout = inflateScheduleDialogLayout(R.layout.schedule_detail_dialog_starttime);
+
+        final View futureControlsGrp = layout.findViewById(R.id.schedule_starttime_futureControlsGrp);
+        final RadioGroup radioGrp = (RadioGroup) layout.findViewById(R.id.schedule_starttime_radiogrp);
+        final RadioButton radioNow = (RadioButton) layout.findViewById(R.id.schedule_starttime_radionow);
+        final TextView dateInfo = (TextView) layout.findViewById(R.id.schedule_starttime_dateinfo);
+        final TextView timeInfo = (TextView) layout.findViewById(R.id.schedule_starttime_timeinfo);
+        final Button dateBut = (Button) layout.findViewById(R.id.schedule_starttime_datebut);
+        final Button timeBut = (Button) layout.findViewById(R.id.schedule_starttime_timebut);
+
+        // TODO: to be updated
+        Calendar now = Calendar.getInstance();
+        dateInfo.setText(new SimpleDateFormat("EEE, d MMM yyyy").format(now.getTime()));
+        timeInfo.setText(new SimpleDateFormat("h:mm a").format(now.getTime()));
+
+        // init
+        radioNow.setSelected(true);
+        futureControlsGrp.setVisibility(View.INVISIBLE);
+
+        radioGrp.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                boolean futureSelected = checkedId == R.id.schedule_starttime_radiofuture;
+                futureControlsGrp.setVisibility(futureSelected ? View.VISIBLE : View.INVISIBLE);
+            }
+        });
+
+        dateBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // TODO: change this
+                Calendar onClickTime = Calendar.getInstance();
+                
+                DatePickerDialog dateDialog = new DatePickerDialog(ScheduleDetailActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth){
+                                Calendar c= Calendar.getInstance();
+                                c.set(Calendar.YEAR,year);
+                                c.set(Calendar.MONTH, monthOfYear);
+                                c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                                dateInfo.setText(new SimpleDateFormat("EEE, d MMM yyyy").format(c.getTime()));
+                            }},
+                            // init with current time
+                            onClickTime.get(Calendar.YEAR),onClickTime.get(Calendar.MONTH),onClickTime.get(Calendar.DAY_OF_MONTH));
+                // dateDlg.updateDate(2012,12,25);
+                dateDialog.show();
+            }
+        });
+        
+        timeBut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // TODO: change this
+                Calendar onClickTime = Calendar.getInstance();
+
+                TimePickerDialog timeDialog = new TimePickerDialog(ScheduleDetailActivity.this,
+                        new TimePickerDialog.OnTimeSetListener() {
+                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                Calendar c= Calendar.getInstance();
+                                c.set(Calendar.HOUR_OF_DAY,hourOfDay);
+                                c.set(Calendar.MINUTE, minute);
+                                timeInfo.setText(new SimpleDateFormat("h:mm a").format(c.getTime()));
+
+                            }
+                        }, onClickTime.get(Calendar.HOUR_OF_DAY), onClickTime.get(Calendar.MINUTE), false);
+                timeDialog.show();
+            }
+        });
+
+        AlertDialog.Builder builder = getAlertDialogBuilder();
+        builder.setView(layout);
+        builder.setTitle("Configure Start Time");
+
+        builder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                //MyActivity.this.finish();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.cancel();
+            }
+        });
+
+        builder.create().show();
+
+    }
 
     public void configureRepetition(View v) {
 
-        ContextThemeWrapper ctw = new ContextThemeWrapper(this, R.style.PinglyDialogTheme);
-        View layout = v.inflate(ctw, R.layout.schedule_detail_dialog_repetition, (ViewGroup) getCurrentFocus());
-
+        View layout = inflateScheduleDialogLayout(R.layout.schedule_detail_dialog_repetition);
+        
         // get refs
         final Spinner repUnitSpinner = (Spinner) layout.findViewById(R.id.scheduler_repetition_unit);
         final SeekBar seekBar = (SeekBar) layout.findViewById(R.id.scheduler_repetition_freq);
@@ -38,7 +150,7 @@ public class ScheduleDetailActivity extends BasePinglyActivity {
 
         // attach list to spinner
         IdValuePair[] spinnerElems = SchedulerRepetitionUnit.toSpinnerValueArray(this);
-        ArrayAdapter<IdValuePair> spinnerArrayAdapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, spinnerElems);
+        ArrayAdapter<IdValuePair> spinnerArrayAdapter = new ArrayAdapter<IdValuePair>(this, android.R.layout.simple_spinner_item, spinnerElems);
         repUnitSpinner.setAdapter(spinnerArrayAdapter);
 
         // init state (will trigger setOnItemSelectedListener, do dependent init there)
@@ -59,6 +171,7 @@ public class ScheduleDetailActivity extends BasePinglyActivity {
                         unit.getResourceNameForSummary(), seekBar.getProgress() + 1));
                                 
             }
+
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
@@ -86,7 +199,7 @@ public class ScheduleDetailActivity extends BasePinglyActivity {
         });
 
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(ctw);
+        AlertDialog.Builder builder = getAlertDialogBuilder();
         builder.setView(layout);
         builder.setTitle("Configure Frequency");
         builder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
